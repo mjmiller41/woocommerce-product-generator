691e8037b7db0f9a0761b7db054ff28bfe6bf5da
Michael Joseph Miller <michael.joseph.miller@gmail.com>
2026-06-02 14:20:57 -0400

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/woocommerce-product-generator/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
